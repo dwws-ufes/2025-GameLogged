a1bc39736ab6bc3022caebda3ea633ee583db0e2
Gianluca <gianluca.mantovaneli@edu.ufes.br>
2025-06-26 19:49:06 -0300

    feat - logout button

--- gamelogged-frontend/src/pages/perfil/PerfilPage.tsx
+++ gamelogged-frontend/src/pages/perfil/PerfilPage.tsx
@@ -1,10 +1,11 @@
 import "./PerfilPage.css"
 function PerfilPage() {
   return (
-    <div className={"perfil-header"}>
-      <p>Esta é a página de perfil do usuário.</p>
-        <img src={"https://github.com/shadcn.png"} alt={"avatar image"} />
-    </div>
+      <div className={"perfil-header"}>
+          <p>Esta é a página de perfil do usuário.</p>
+          <img src={"https://github.com/shadcn.png"} alt={"avatar image"} />
+      </div>
+
   );
 }
 export default PerfilPage;
