d479a26dc1a60c9b5f6f225510a8486e2bfcb800
Gianluca <gianluca.mantovaneli@edu.ufes.br>
2025-06-28 12:01:43 -0300

    feat - paginas de cada aba do perfil

---
+++ gamelogged-frontend/src/components/ui/profileTabs/FriendTab.tsx
@@ -0,0 +1,11 @@
+function FriendTab() {
+    return (
+        <div className="FriendTab">
+            <h2>Friend Tab</h2>
+            <p>This is the Friend tab content.</p>
+            {/* Add your friend-related content here */}
+        </div>
+    );
+}
+
+export default FriendTab;
--- gamelogged-frontend/src/pages/perfil/PerfilPage.tsx
+++ gamelogged-frontend/src/pages/perfil/PerfilPage.tsx
@@ -1,32 +1,58 @@
-import "./PerfilPage.css"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-function PerfilPage() {
-  return (
-      <div className={"Perfil-page"}>
-          <div className={"Perfil-page-header-left"}>
-              <img className="Perfil-page-avatar" src={"https://github.com/shadcn.png"} alt={"avatar image"} />
-              <p className="Perfil-page-user-name">
-                  Nome Usuario
-              </p>
-              <button className="Perfil-edit-button" >
-                  Editar Perfil
-              </button>
-          </div>
-          
-          <Tabs defaultValue="account" className="Perfil-page-tabs">
-              <TabsList>
-                  <TabsTrigger value="profile">Profile</TabsTrigger>
-                  <TabsTrigger value="games">Games</TabsTrigger>
-                  <TabsTrigger value="reviews">Reviews</TabsTrigger>
-                  <TabsTrigger value="friends">Friends</TabsTrigger>
-              </TabsList>
-              <TabsContent value="profile">Bio, jogos jogados recentemente .</TabsContent>
-              <TabsContent value="games">Jogos que o usuario interagiu.</TabsContent>
-              <TabsContent value="reviews">Lista de todos os reviews feitos pelo usuario.</TabsContent>
-              <TabsContent value="friends">Lista de amigos </TabsContent>
-          </Tabs>
-      </div>
+import "./PerfilPage.css";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import ProfileTab from "@/components/ui/profileTabs/ProfileTab";
+import GameTab from "@/components/ui/profileTabs/GameTab";
+import ReviewTab from "@/components/ui/profileTabs/ReviewTab";
+import FriendTab from "@/components/ui/profileTabs/FriendTab";
 
-  );
+function PerfilPage() {
+    return (
+        <div className="Perfil-page">
+            <div className="Perfil-page-header-left">
+                <img
+                    className="Perfil-page-avatar"
+                    src="https://github.com/shadcn.png"
+                    alt="avatar image"
+                />
+                <p className="Perfil-page-user-name">Nome Usuario</p>
+                <button className="Perfil-edit-button">Editar Perfil</button>
+            </div>
+
+            <Tabs defaultValue="profile" className="Perfil-page-tabs">
+                //TODO: Colocar icones nas abas
+                <TabsList>
+                    <TabsTrigger value="profile">
+                        Profile
+                    </TabsTrigger>
+                    <TabsTrigger value="games">
+                        Games
+                    </TabsTrigger>
+                    <TabsTrigger value="reviews">
+                        Reviews
+                    </TabsTrigger>
+                    <TabsTrigger value="friends">
+                        Friends
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="profile">
+                    <ProfileTab/>
+                </TabsContent>
+
+                <TabsContent value="games">
+                    <GameTab/>
+                </TabsContent>
+
+                <TabsContent value="reviews">
+                    <ReviewTab/>
+                </TabsContent>
+
+                <TabsContent value="friends">
+                    <FriendTab/>
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
 }
+
 export default PerfilPage;
