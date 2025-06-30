a836c8685201f6802e590cc3c11260ee80a46dfd
Joao <joao.g.rocha@edu.ufes.br>
2025-06-30 16:53:31 -0300

    feat - Loading

--- gamelogged-frontend/src/game/controllers/GameController.ts
+++ gamelogged-frontend/src/game/controllers/GameController.ts
@@ -1,19 +1,28 @@
+import { AuthController } from "@/authentication/controllers/AuthController";
 import { gameAPI } from "@/services/APIService";
+import { toast } from "sonner";
 
- export class GameController {
-   private static instance: GameController;
-   
-   public static getInstance(): GameController {
-     if (!GameController.instance) {
-       GameController.instance = new GameController();
-     }
-     return GameController.instance;
-   }
+export class GameController {
+    private static instance: GameController;
+    public isUserAuthenticated: boolean = false;
+    
+
+    constructor() {
+        this.isUserAuthenticated = AuthController.getInstance().isAuthenticated();
+        
+    }
+
+    public static getInstance(): GameController {
+        if (!GameController.instance) {
+            GameController.instance = new GameController();
+        }
+        return GameController.instance;
+    }
 
 
-   public async searchGameByName(name: string): Promise<any> {
-        return await gameAPI.fetchGameByName(name); 
-   }
+    public async searchGameByName(name: string): Promise<any> {
+        let game = await gameAPI.fetchGameByName(name);
+        return game;
+    }
 
-   
- }
+}
